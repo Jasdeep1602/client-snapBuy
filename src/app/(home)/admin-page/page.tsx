a4408e9dc72f83658352b7838ab6e8f3e8c38518
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2024-06-21 20:30:46 +0530

    input bug

--- src/app/(home)/admin-page/page.tsx
+++ src/app/(home)/admin-page/page.tsx
@@ -130,9 +130,10 @@ export default function CreateProduct() {
                     name="product_id"
                     id="product_id"
                     value={productdetails?.product_id}
-                    // autoComplete="Product ID"
+                    autoComplete="Product ID"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     onChange={handleInputChange}
+                    placeholder=""
                     required
                   />
                 </div>
